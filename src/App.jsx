6d9a3cd9f278ecdc8b69b135fc5838be6cf935a7
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-11-05 20:05:40 +0500

    to create a few changes in a app.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import TodoApp from './components/TodoApp'
-  import { ToastContainer} from 'react-toastify';
+import React from "react";
+import TodoApp from "./components/TodoApp";
+import { ToastContainer } from "react-toastify";
 const App = () => {
   return (
-    <div className='bg-gray-200 min-h-screen py-6'>
-      <h1 className='text-center font-serif font-semibold text-gray-500 mb-4'>Todo List App</h1>
-      <TodoApp/>
-      <ToastContainer/>
+    <div className="bg-gray-200 min-h-screen py-6">
+      <h1 className="text-center font-serif font-semibold text-gray-500 mb-4">
+        Todo List App
+      </h1>
+      <TodoApp />
+      <ToastContainer />
     </div>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
