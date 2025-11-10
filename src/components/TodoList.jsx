ab24fd54a55842a47f37a21df94b33f105c5b4e8
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-11-10 16:41:10 +0500

    Make a few changes in all components and TodoItem and TodoList in todo app

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,29 +2,30 @@ import React from "react";
 import TodoItem from "./TodoItem";
 
 const TodoList = ({ todos, deleteTodo, toggleComplete, editTodo }) => {
-
   return (
-   <div className=" ">
-     <div className="overflow-y-scroll h-[60vh] custom-scrollbar-style mx-auto"> 
-      <div className="md:w-6/12 mx-auto mt-6 space-y-3"> 
+    <div className=" ">
+      <div className="overflow-y-scroll h-[60vh] custom-scrollbar-style mx-auto"> 
         {todos.length === 0 ? (
-          <p className="text-center text-gray-500 p-4 bg-white rounded-lg shadow-md text-break">
-            No tasks yet! Add a new item above.
-          </p>
+          <div className="md:w-6/12 mx-auto mt-6 p-4">
+            <p className="text-center text-gray-500 p-4 bg-white rounded-lg shadow-md text-break w-full">
+              No tasks yet! Add a new item above.
+            </p>
+          </div>
         ) : (
-          todos.map((todo) => (
-            <TodoItem
-              key={todo.id}
-              todo={todo}
-              deleteTodo={deleteTodo}
-              toggleComplete={toggleComplete}
-              editTodo={editTodo}
-            />
-          ))
+          <div className="md:w-6/12 mx-auto mt-6 space-y-3 grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
+            {todos.map((todo) => (
+              <TodoItem
+                key={todo.id}
+                todo={todo}
+                deleteTodo={deleteTodo}
+                toggleComplete={toggleComplete}
+                editTodo={editTodo}
+              />
+            ))}
+          </div>
         )}
       </div>
     </div>
-   </div>
   );
 };
 
