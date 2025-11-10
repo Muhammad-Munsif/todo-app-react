25eda2a3e47598ed167251f28ad9d7e189f06ece
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-11-10 11:19:21 +0500

    Make a few changes in all components in todo app

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,22 +4,30 @@ import TodoItem from "./TodoItem";
 const TodoList = ({ todos, deleteTodo, toggleComplete, editTodo }) => {
 
   return (
-    <div className="md:w-6/12  mx-auto mt-6 space-y-3 overflow-auto ">
-      {todos.length === 0 ? (
-        <p className="text-center text-gray-500 p-4 bg-white rounded-lg shadow-md text-break">
-          No tasks yet! Add a new item above.
-        </p>
-      ) : (
-        todos.map((todo) => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            deleteTodo={deleteTodo}
-            toggleComplete={toggleComplete}
-            editTodo={editTodo}
-          />
-        ))
-      )}
+    // 1. THIS is the scrollable container. It MUST have a defined height.
+    <div className="overflow-y-scroll 
+                  h-[60vh] 
+                  custom-scrollbar-style mx-auto"> 
+      
+      {/* 2. This inner div holds the content (centered, spaced). 
+          We removed 'overflow-auto' from here as the parent manages scrolling. */}
+      <div className="md:w-6/12 mx-auto mt-6 space-y-3"> 
+        {todos.length === 0 ? (
+          <p className="text-center text-gray-500 p-4 bg-white rounded-lg shadow-md text-break">
+            No tasks yet! Add a new item above.
+          </p>
+        ) : (
+          todos.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              deleteTodo={deleteTodo}
+              toggleComplete={toggleComplete}
+              editTodo={editTodo}
+            />
+          ))
+        )}
+      </div>
     </div>
   );
 };
